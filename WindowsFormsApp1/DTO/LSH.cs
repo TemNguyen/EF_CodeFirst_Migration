657d926f53f8b2abbd1831c7405c4f745ef3bd60
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-07-16 12:24:49 +0700

    first commit

---
+++ WindowsFormsApp1/DTO/LSH.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.DTO
+{
+    public class LSH
+    {
+        [Key]
+        public int ID_Lop { get; set; }
+        public string NameLop { get; set; }
+
+        public ICollection<SV> SVs { get; set; }
+        public LSH()
+        {
+            SVs = new HashSet<SV>();
+        }
+
+        public override string ToString()
+        {
+            return NameLop;
+        }
+    }
+}
